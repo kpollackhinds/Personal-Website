bf76d59690d6c12c861783997f2b38b87afbf746
kpollackhinds <kahlil.pollack-hinds@stonybrook.edu>
2023-08-17 23:57:11 -0400

    made changes to nav bar and added additional page for resume

--- portfolio.py
+++ portfolio.py
@@ -14,6 +14,8 @@ def projects():
 def about_me():
     return render_template("aboutme.html")
 
-
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 if __name__ == "__main__":
     app.run(debug=True)
